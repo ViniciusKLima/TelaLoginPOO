a52c10086f16cff043ff6a9ae306162b8be39128
ViniciusKLima <viniciuskls2006@gmail.com>
2024-11-25 16:20:46 -0300

    livro att

--- js/index.js
+++ js/index.js
@@ -1,20 +1,14 @@
-var formSignin = document.querySelector('#signin')
-var formSignup = document.querySelector('#signup')
-var btnColor = document.querySelector('.btnColor')
+function mostrarConteudo(secao) {
+  // Esconde todas as seções
+  var conteudos = document.querySelectorAll('.conteudo');
+  conteudos.forEach(function(conteudo) {
+      conteudo.style.display = 'none';
+  });
 
-document.querySelector('#btnSignin')
-  .addEventListener('click', () => {
-    formSignin.style.left = "25px"
-    formSignup.style.left = "450px"
-    btnColor.style.left = "0px"
-})
-
-document.querySelector('#btnSignup')
-  .addEventListener('click', () => {
-    formSignin.style.left = "-450px"
-    formSignup.style.left = "25px"
-    btnColor.style.left = "110px"
-})
+  // Exibe a seção escolhida
+  var secaoEscolhida = document.getElementById(secao);
+  secaoEscolhida.style.display = 'block';
+}
 
 function cadastrarUsuario(event) {
   event.preventDefault();
